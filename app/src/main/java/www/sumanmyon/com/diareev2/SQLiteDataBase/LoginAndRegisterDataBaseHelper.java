a072d1107a4537df220632bf11470e1fbdc8b1c6
Suman Neupane <sumanmyon.sam@gmail.com>
2019-04-06 19:23:37 +0545

    Login and register table created

--- app/src/main/java/www/sumanmyon/com/diareev2/SQLiteDataBase/LoginAndRegisterDataBaseHelper.java
+++ app/src/main/java/www/sumanmyon/com/diareev2/SQLiteDataBase/LoginAndRegisterDataBaseHelper.java
@@ -7,7 +7,7 @@ import android.support.annotation.Nullable;
 
 public class LoginAndRegisterDataBaseHelper extends SQLiteOpenHelper {
     //private String DATABASE_NAME = getDatabaseName();
-    private static final String DATABASE_TABLE ="loginnregister_table";
+    private static final String LOGINnREGISTER_TABLE ="loginnregister_table";
     private static final String ID = "id";
     private static final String username = "username";
     private static final String password = "password";
@@ -19,11 +19,18 @@ public class LoginAndRegisterDataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String createTableLoginAndRegister = "create table "+LOGINnREGISTER_TABLE+
+                "("+ID+" INTEGER PRIMARY KEY AUTOINCREMENT ,"+
+                username+" TEXT ,"+password+" TEXT"+")";
+        db.execSQL(createTableLoginAndRegister);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        String dropLoginAndRegisterTable = "DROP TABLE IF EXISTS "+LOGINnREGISTER_TABLE;
+        db.execSQL(dropLoginAndRegisterTable);
+        onCreate(db);
     }
+
+
 }
