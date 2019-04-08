f349c81ce0d9c63c7d25baf168d92a1aad0464c5
Suman Neupane <sumanmyon.sam@gmail.com>
2019-04-08 11:23:32 +0545

    Implementing register interface in register view

--- app/src/main/java/www/sumanmyon/com/diareev2/Register/View/RegisterActivity.java
+++ app/src/main/java/www/sumanmyon/com/diareev2/Register/View/RegisterActivity.java
@@ -2,13 +2,14 @@ package www.sumanmyon.com.diareev2.Register.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
 import www.sumanmyon.com.diareev2.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements RegisterView {
 
     private EditText fullNameEditText, emailEditText, addressEditText, phoneNoEditText, userNameEditText, passwordEditText, re_passwordEditText;
     private CheckBox acceptCheckBox;
@@ -33,6 +34,56 @@ public class RegisterActivity extends AppCompatActivity {
 
         //todo Binding views
         bindingViews();
+
+    }
+
+    @Override
+    public void onSuccess(String message) {
+        
+    }
+
+    @Override
+    public void onFailure(String message) {
+
+    }
+
+    @Override
+    public void setFullName(String message) {
+
+    }
+
+    @Override
+    public void setEmail(String message) {
+
+    }
+
+    @Override
+    public void setAddress(String message) {
+
+    }
+
+    @Override
+    public void setPhoneNo(String message) {
+
+    }
+
+    @Override
+    public void setUserName(String message) {
+
+    }
+
+    @Override
+    public void setPassword(String message) {
+
+    }
+
+    @Override
+    public void setRePassword(String message) {
+
+    }
+
+    @Override
+    public void setCheckAccept(String message) {
 
     }
 }
