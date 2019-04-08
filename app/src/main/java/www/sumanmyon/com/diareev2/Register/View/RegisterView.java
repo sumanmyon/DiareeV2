50b8428fa77e125b116c3cf6323ba412f8687a2c
Suman Neupane <sumanmyon.sam@gmail.com>
2019-04-08 11:21:12 +0545

    Setting register view interface

--- app/src/main/java/www/sumanmyon/com/diareev2/Register/View/RegisterView.java
+++ app/src/main/java/www/sumanmyon/com/diareev2/Register/View/RegisterView.java
@@ -1,5 +1,17 @@
 package www.sumanmyon.com.diareev2.Register.View;
 
 public interface RegisterView {
+    void onSuccess(String message);
+    void onFailure(String message);
+
+    void setFullName(String message);
+    void setEmail(String message);
+    void setAddress(String message);
+    void setPhoneNo(String message);
+    void setUserName(String message);
+    void setPassword(String message);
+    void setRePassword(String message);
+
+    void setCheckAccept(String message);
 
 }
